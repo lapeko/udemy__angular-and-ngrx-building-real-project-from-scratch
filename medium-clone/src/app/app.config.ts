0d3a4dcdfac1c8b66aaa1ea723f0eca3a8f0917d
lapeko <lapeko88@gmail.com>
2023-11-18 16:14:36 +0100

    use more stable version of angular

--- medium-clone/src/app/app.config.ts
+++ medium-clone/src/app/app.config.ts
@@ -1,12 +1,14 @@
-import {ApplicationConfig} from '@angular/core'
+import {ApplicationConfig, isDevMode} from '@angular/core'
 import {provideRouter} from '@angular/router'
 import {provideStore} from '@ngrx/store'
 
-import {appRoutes} from './app.routes'
+import {appRoutes} from './app.routes';
+import { provideStoreDevtools } from '@ngrx/store-devtools'
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(appRoutes),
     provideStore(),
-  ]
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })
+]
 }
