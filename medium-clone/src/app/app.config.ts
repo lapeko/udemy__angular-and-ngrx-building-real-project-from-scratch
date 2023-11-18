887b864a953a7ee3aa0acd025f7e7b40b0f087cb
lapeko <lapeko88@gmail.com>
2023-11-18 16:26:27 +0100

    set up ngrx-store-devtools

--- medium-clone/src/app/app.config.ts
+++ medium-clone/src/app/app.config.ts
@@ -1,14 +1,20 @@
 import {ApplicationConfig, isDevMode} from '@angular/core'
 import {provideRouter} from '@angular/router'
 import {provideStore} from '@ngrx/store'
+import {provideStoreDevtools} from '@ngrx/store-devtools'
 
-import {appRoutes} from './app.routes';
-import { provideStoreDevtools } from '@ngrx/store-devtools'
+import {appRoutes} from './app.routes'
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(appRoutes),
     provideStore(),
-    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })
-]
+    provideStoreDevtools({
+      maxAge: 25,
+      logOnly: !isDevMode(),
+      autoPause: true,
+      trace: true,
+      traceLimit: 75
+    })
+  ]
 }
